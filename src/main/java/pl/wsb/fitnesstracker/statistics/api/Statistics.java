4b0dce657803b2ae07d6c7d363f7117dce05917d
zdinozi <zdi724@gmail.com>
2026-03-28 13:27:11 +0100

    Lab3_Zad7 - database entities update

--- src/main/java/pl/wsb/fitnesstracker/statistics/api/Statistics.java
+++ src/main/java/pl/wsb/fitnesstracker/statistics/api/Statistics.java
@@ -6,6 +6,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import pl.wsb.fitnesstracker.user.api.User;
 
 @Entity
 @Table(name = "Statistics")
@@ -19,13 +20,21 @@ public class Statistics {
     @Nullable
     private Long id;
 
+    @OneToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "total_trainings", nullable = false)
     private int totalTrainings;
 
+    @Column(name = "total_distance", nullable = false)
     private double totalDistance;
 
+    @Column(name = "total_calories_burned", nullable = false)
     private int totalCaloriesBurned;
 
-    public Statistics(int totalTrainings, double totalDistance, int totalCaloriesBurned) {
+    public Statistics(User user, int totalTrainings, double totalDistance, int totalCaloriesBurned) {
+        this.user = user;
         this.totalTrainings = totalTrainings;
         this.totalDistance = totalDistance;
         this.totalCaloriesBurned = totalCaloriesBurned;
