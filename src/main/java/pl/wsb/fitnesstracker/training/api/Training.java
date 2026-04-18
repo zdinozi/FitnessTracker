2c42b694c21bc83e13f502d6afa97d06bdd9fabe
zdinozi <zdi724@gmail.com>
2026-04-18 13:36:32 +0200

    revert merge change

--- src/main/java/pl/wsb/fitnesstracker/training/api/Training.java
+++ src/main/java/pl/wsb/fitnesstracker/training/api/Training.java
@@ -23,7 +23,7 @@ public class Training {
     @Nullable
     private Long id;
 
-    @ManyToOne(optional = false)
+    @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
